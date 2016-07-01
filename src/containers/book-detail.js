f9b7ff581f3f0c78f961a1dba8b9f41e70716acf
Mike Ulvila <mikeulvila@me.com>
2016-07-01 15:44:07 -0500

    create book detail container

---
+++ src/containers/book-detail.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {selectBook} from '../actions/index';
+import {bindActionCreators} from 'redux';
+
+class BookDetail extends Component {
+  render() {
+    return (
+      <div>Book Detail</div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    book: state.activeBook
+  };
+}
+
+export default connect(mapStateToProps)(BookDetail);
